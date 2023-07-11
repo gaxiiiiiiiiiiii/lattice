c77526dcd6d9d3975abf416b92d0b574a6366e46
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2023-07-11 15:20:41 +0900

    一応bilatticeは定義した。継承が上手くできてない。

--- AlgebraicLattice/Bilattice.v
+++ AlgebraicLattice/Bilattice.v
@@ -1,6 +1,6 @@
 Require Export CompLat.
 
-Record bilattice := {
+Record bilattice_ := {
   base :> Type;
 
   meett : base -> base -> base;
@@ -11,7 +11,7 @@ Record bilattice := {
   joinAt : forall a b c, joint a (joint b c) = joint (joint a b) c;
   joinKt : forall a b, meett a (joint a b) = a;
   meetKt : forall a b, joint a (meett a b) = a;
-
+  
   meetk : base -> base -> base;
   joink : base -> base -> base;
   meetCk : forall a b, meetk a b = meetk b a;
@@ -22,7 +22,7 @@ Record bilattice := {
   meetKk : forall a b, joink a (meetk a b) = a;
 }.
 
-Definition latticet (L : bilattice) : lattice := {|
+Definition latticet (L : bilattice_) : lattice := {|
   meetC := meetCt L;
   joinC := joinCt L;
   meetA := meetAt L;
@@ -31,7 +31,7 @@ Definition latticet (L : bilattice) : lattice := {|
   meetK := meetKt L;
 |}.
 
-Definition latticek (L : bilattice) : lattice := {|
+Definition latticek (L : bilattice_) : lattice := {|
   meetC := meetCk L;
   joinC := joinCk L;
   meetA := meetAk L;
@@ -40,8 +40,8 @@ Definition latticek (L : bilattice) : lattice := {|
   meetK := meetKk L;
 |}.
 
-Record bicomplattice := {
-  blat : bilattice;
+Record bilattice := {
+  blat :> bilattice_;
 
   latt := latticet blat;  
   supt : Ensemble latt -> latt;
@@ -52,8 +52,7 @@ Record bicomplattice := {
   is_lowbt : forall (A : Ensemble latt) a, a \in A -> (inft A) ≺ a;
   is_inft : forall (A : Ensemble latt) a,
            (forall b, b \in A -> a ≺ b) -> a ≺ (inft A);
-  bott := inft (Full_set latt);
-  topt := supt (Full_set latt);
+
 
   latk := latticet blat;  
   supk : Ensemble latk -> latk;
@@ -64,22 +63,53 @@ Record bicomplattice := {
   is_lowbk : forall (A : Ensemble latk) a, a \in A -> (infk A) ≺ a;
   is_infk : forall (A : Ensemble latk) a,
            (forall b, b \in A -> a ≺ b) -> a ≺ (infk A);
-  botk := infk (Full_set latk);
-  topk := supk (Full_set latk);
 
+
+  neg : blat -> blat;
+  leRt : forall x y, @le latt  x y -> @le latt (neg y) (neg x);
+  leRk : forall x y, @le latk x y -> @le latk (neg x) (neg y);
+  NN : forall x, neg (neg x) = x;
 }.
 
-Definition complatt (L : bicomplattice) := {|
+Definition complatt (L : bilattice) := {|
   is_upb := is_upbt L;
   is_sup := is_supt L;
   is_lowb := is_lowbt L;
   is_inf := is_inft L;
 |}.
 
-Definition complatk (L : bicomplattice) := {|
+Definition complatk (L : bilattice) := {|
   is_upb := is_upbk L;
   is_sup := is_supk L;
   is_lowb := is_lowbk L;
   is_inf := is_infk L;
 |}.
+
+
+
+
+Section misc.
+
+  Variable L : bilattice.
+  Notation T := (top (latt L)).
+  Notation F := (bot (latt L)).
+  (* Notation "⊤" := (@topk L). *)
+  (* Notation "⊥" := (@botk L). *)
+  Notation "x ≺_t y" := (@le (latt L) x y)(at level 30).
+  Notation "x ≺_k y" := (@le (latk L) x y)(at level 30).
+  Notation "x ∧ y" := (@meett L x y)(at level 20).
+  Notation "x ∨ y" := (@joint L x y)(at level 20).
+  Notation "x <*> y" := (@meetk L x y)(at level 20).
+  Notation "x <+> y" := (@joink L x y)(at level 20).
+  Notation "¬ x" := (@neg L x)(at level 10).
+
+  Lemma neg_false_true : 
+    ¬ T = F.
+  Proof.
+    Check antisym.
+
+
+
+
+
 
